0811e189f0c0bd10f1e090b9c3b6b5ededd55071
Bogdan191 <bogdanpintilie00@yahoo.com>
2020-12-27 15:52:26 +0200

    Adding files for the first time

---
+++ Controllers/HomePageController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project.NET.Controllers
+{
+    public class HomePageController : Controller
+    {
+        // GET: HomePage
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public string About()
+        {
+            return "this is a custom About page!";
+        }
+
+        
+    }
+}
